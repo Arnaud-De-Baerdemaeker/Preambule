344a6ac49455f6a42afba0420c6637ac388313f2
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-03-16 14:11:14 +0100

    Preapre ACF fields for Our producers' page and put the mini hero

---
+++ wp-content/themes/storefront-child/template-tasting.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template name: Tasting
+ *
+ * @package storefront
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+get_template_part('templates/mini-hero');
+?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="container">
+
+			
+
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
+?>
