05890b2afe24dbf1de5ec2b089eb556aa32f62e8
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-03-18 10:37:59 +0100

    Prepare the tasting page

--- wp-content/themes/storefront-child/template-tasting.php
+++ wp-content/themes/storefront-child/template-tasting.php
@@ -15,7 +15,9 @@ get_template_part('templates/mini-hero');
 	<main id="main" class="site-main" role="main">
 		<div class="container">
 
-			
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at faucibus lorem. Vivamus imperdiet vitae nisl quis feugiat. Duis laoreet posuere nisl sed dapibus. Donec et arcu elementum, finibus eros a, ullamcorper ligula. Etiam cursus enim ut fermentum pharetra. Quisque sed quam lobortis, euismod sem in, ornare augue. Phasellus sed risus ultrices, dignissim felis non, cursus sapien. Quisque dapibus, metus et bibendum varius, neque turpis condimentum arcu, sit amet sodales eros massa at dolor. Nam tempor tincidunt fringilla. Fusce rutrum et mauris auctor rutrum.
+
+		Nunc fermentum velit nec justo lobortis, vitae tincidunt tellus dignissim. Pellentesque malesuada, metus sed imperdiet porttitor, magna mi porta est, ut ornare nibh velit ac augue. Curabitur sed mi mi. Nam sit amet malesuada lectus. Maecenas sed ipsum ornare, efficitur erat ut, vestibulum elit. Duis sollicitudin aliquam odio, ac rhoncus neque fermentum in. Aenean bibendum porta cursus. Aliquam non diam vel arcu ultricies vulputate. Sed maximus eros eget ipsum accumsan, at volutpat lorem fringilla.
 
 		</div>
 	</main><!-- #main -->
